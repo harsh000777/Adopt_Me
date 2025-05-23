3a346f940296e4f6593d892ddb9ebec2e5de269a
harsh000777 <160133573+harsh000777@users.noreply.github.com>
2025-05-23 12:47:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/adopt-me-react/', 
+  base: '/Adopt_Me/', 
   plugins: [react()],
 })
